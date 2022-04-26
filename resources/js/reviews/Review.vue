e8762f09b5c52cb67c50c6c139914d07e03c8ecf
izaakdale <izaakdale@live.com>
2022-04-26 15:07:06 -0700

    Show booking on review page

--- resources/js/reviews/Review.vue
+++ resources/js/reviews/Review.vue
@@ -1,30 +1,50 @@
 <template>
-    <div>
-        <div v-if="loading">
-            loading..
-        </div>
-        <div v-else>
-            <div v-if="alreadyReviewed">
-                <h3>
-                    Review already submitted for this booking...
-                </h3>
+    <div class="row">     
+         <div :class="[{'col-md-8': loading || !alreadyReviewed}, {'d-md-12': !loading && alreadyReviewed }]">
+            <div v-if="loading">
+                loading...
             </div>
             <div v-else>
-                <div class="form-group">
-                    <label class="text-muted" for="">Select Star Rating</label>
-                    <star-rating
-                        class="fa-3x"
-                        v-model="review.rating"
-                    ></star-rating>
+                <div v-if="alreadyReviewed">
+                    <h3>
+                        Review already submitted for this booking...
+                    </h3>
                 </div>
-                <div class="form-group">
-                    <label for="content" class="text-muted">Describe your experience</label>
-                    <textarea name="content" cols="30" rows="10" class="form-control" v-model="review.content"></textarea>
+                <div v-else>
+                    <div class="form-group">
+                        <label class="text-muted" for="">Select Star Rating</label>
+                        <star-rating
+                            class="fa-3x"
+                            v-model="review.rating"
+                        ></star-rating>
+                    </div>
+                    <div class="form-group">
+                        <label for="content" class="text-muted">Describe your experience</label>
+                        <textarea name="content" cols="30" rows="10" class="form-control" v-model="review.content"></textarea>
+                    </div>
+                    <button class="btn btn-md btn-primary col-md-12">Submit</button>
                 </div>
-
-                <button class="btn btn-md btn-primary col-md-12">Submit</button>
             </div>
         </div>
+        <div :class="[{'col-md-4': loading || !alreadyReviewed}, {'d-none': !loading && alreadyReviewed }]">
+            <div v-if="loading">
+                loading..
+            </div>
+            <div v-else>
+                <div v-if="booking" class="card">
+                    <div class="card-body">
+                        <h3>
+                            Your stay...
+                        </h3>
+                        <hr>
+                        <article>
+                            <router-link :to="{name: 'Bookable', params: {id: booking.bookable.id}}">{{ booking.bookable.title }}</router-link>
+                            From {{ booking.from }} to {{ booking.to }}
+                        </article>
+                    </div>
+                </div>
+            </div>
+        </div>  
     </div>
 </template>
 
@@ -56,12 +76,14 @@ export default {
         }).catch(error => {
             return axios.get(`/api/bookingByReview/${this.$route.params.id}`).then(response => {
                 this.booking = response.data.data;
+                console.log('Booking')
+                console.log(this.booking);
+            })
+            // will probably want to catch this, here we have the case where an invalid reviewKey has been entered
+            // which means booking cannot be found/doesnt exist. Causes not stop loading
+            .catch(error => {
+                this.booking = null;
             });
-            // // will probably want to catch this, here we have the case where an invalid reviewKey has been entered
-            // // which means booking cannot be found/doesnt exist. Causes not stop loading
-            // .catch(error => {
-            //     this.booking = null;
-            // });
         }).then(() => {
             this.loading = false;
         });
